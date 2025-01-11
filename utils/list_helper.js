55b643ed4e687b46f847a7c1efddb09bf2d419a7
Karen <karen.nikoghosyan.her@gmail.com>
2025-01-11 22:00:48 +0400

    4.5*: Helper Functions and Unit Tests, step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -11,7 +11,33 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if(blogs.length === 0) {
+    return null
+  }
+  /* let favorite = blogs[0]
+
+  blogs.forEach((blog) => {
+    if(blog.likes && blog.likes > favorite.likes) {
+      favorite = blog
+    }
+  })
+  return favorite  */
+  
+  const favorite = blogs.reduce((prev, cur) => {
+    return (cur.likes || 0) > (prev.likes || 0) ? cur : prev
+
+  })
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
