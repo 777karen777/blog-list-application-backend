37ddc0156cdcaa9a4f2be4f34723e12d6d4cf068
Karen <karen.nikoghosyan.her@gmail.com>
2025-01-11 23:23:09 +0400

    4.7*: Helper Functions and Unit Tests, step 5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -36,8 +36,54 @@ const favoriteBlog = (blogs) => {
   }
 }
 
+const mostBlogs = (blogs) => {
+  const blogsCount = {}
+
+  let maxAutor = null
+  let maxBlogs = 0
+
+  blogs.forEach(blog => {
+    const author = blog.author
+    blogsCount[author] = (blogsCount[author] || 0) + 1
+
+    if(blogsCount[author] > maxBlogs) {
+      maxBlogs = blogsCount[author]
+      maxAutor = author
+    }
+  })
+
+  return {
+    author: maxAutor,
+    count: maxBlogs
+  }
+}
+
+const mostLikes  = (blogs) => {
+  const likesCount = {}
+
+  let maxAutor = null
+  let maxLikes = 0
+
+  blogs.forEach(blog => {
+    const author = blog.author
+    likesCount[author] = (likesCount[author] || 0) + (blog.likes || 0)
+
+    if(likesCount[author] > maxLikes) {
+      maxLikes = likesCount[author]
+      maxAutor = author
+    }
+  })
+
+  return {
+    author: maxAutor,
+    likes: maxLikes
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
