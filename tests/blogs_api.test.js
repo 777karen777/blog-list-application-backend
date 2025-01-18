42e46922a220e1ea8ea6318c8b5c0ee37a4b88ec
Karen <karen.nikoghosyan.her@gmail.com>
2025-01-18 09:21:05 +0400

    4.9: Blog List Tests, step 2

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -6,6 +6,7 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const Blog = require('../models/blog')
+const blog = require('../models/blog')
 
 const api = supertest(app)
 
@@ -30,6 +31,39 @@ describe('API calls', () => {
     const response = await api.get('/api/blogs')
     assert.strictEqual(response.body.length, helper.initialBlogs.length )
   })
+
+  test('The unic identifier property of the blog post is named "id"', async () => {
+    const response = await api.get('/api/blogs')
+    const propNamedId = Object.keys(response.body[0])
+    assert(propNamedId.includes('id') && !propNamedId.includes('_id'))
+  })
+  
+  test('2-The unic identifier property of the blog post is named "id"', async () => {
+    const response = await api.get('/api/blogs')
+    const propNamedId = response.body.map(blog => blog.id)
+    const propNamed_id = response.body.filter(blog => blog._id)
+    assert.strictEqual(propNamedId.length, helper.initialBlogs.length)
+    assert.strictEqual(propNamed_id.length, 0)
+  })
+
+  //Suggested by GPT
+  test('3-The unic identifier property of the blog post is named "id"', async () => {
+    const response = await api.get('/api/blogs')
+    const allHaveID = response.body.every(blog => 'id' in blog)
+    const noneHave_iD = response.body.every(blog => !('_id' in blog))
+    assert(allHaveID && noneHave_iD)
+  })
+  
+  //Suggested by GPT
+  test('4-The unic identifier property of the blog post is named "id"', async () => {
+    const response = await api.get('/api/blogs')
+    response.body.forEach(blog => {
+      assert('id' in blog)
+      assert(!('_id' in blog))
+    });
+  })
+
+  
 
 })
 
